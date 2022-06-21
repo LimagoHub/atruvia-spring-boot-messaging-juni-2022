e35de4ddd13c04a4a933dc805f178dd6d830c235
Jo Wagner <jowagner@limago.de>
2022-06-21 13:12:45 +0200

    Project move

---
+++ smartbank24/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+…
+Usmartbank24-kreditantrag-registrierung/smartbank24-kreditantrag-registrierung/pom.xml,d\6\d678d0ec39e93cac45e9c45fbe367271f66d74d0
---
+++ smartbank24/smartbank24-kreditantrag-registrierung/src/main/java/de/bankenit/smartbank24kreditantragregistrierung/port/handler/inner/KreditantragHandlerImpl.java
@@ -0,0 +1,43 @@
+package de.bankenit.smartbank24kreditantragregistrierung.port.handler.inner;
+
+
+import de.bankenit.smartbank24kreditantragregistrierung.adapter.dto.KreditantragDTO;
+import de.bankenit.smartbank24kreditantragregistrierung.adapter.events.KreditantragRegistriertEvent;
+import de.bankenit.smartbank24kreditantragregistrierung.domain.services.KreditantragService;
+import de.bankenit.smartbank24kreditantragregistrierung.domain.services.KreditantragServiceException;
+import de.bankenit.smartbank24kreditantragregistrierung.port.mapper.KreditantragDTOMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Component
+@AllArgsConstructor
+@Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.READ_UNCOMMITTED, rollbackFor = KreditantragServiceException.class)
+public class KreditantragHandlerImpl implements de.bankenit.smartbank24kreditantragregistrierung.port.handler.KreditantragHandler {
+
+    private static final String DESTINATION ="kreditantrag-registriert-out-0";
+
+    private final StreamBridge bride;
+
+    private final KreditantragService service;
+    private final KreditantragDTOMapper mapper;
+
+
+    //public void handle(KreditantragErfasstEvent dto) throws KreditantragServiceException{
+
+    @Override
+    public void handleKreditantragErfassen(KreditantragDTO dto) throws KreditantragServiceException{
+
+        try {
+            service.neuAnlegen(mapper.convert(dto));
+            bride.send(DESTINATION, KreditantragRegistriertEvent.builder().kreditantrag(dto).build());
+        } catch (KreditantragServiceException e) {
+            // Missevent feurn
+            throw e;
+        }
+    }
+}
