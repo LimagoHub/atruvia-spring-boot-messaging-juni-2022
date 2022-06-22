4b9d0a44c84da440082aa28ffa2c49dc437f37d4
Jo Wagner <jowagner@limago.de>
2022-06-22 10:37:13 +0200

    Scoring

---
+++ smartbank24/smartbank24-kreditantrag-city-scoring/src/test/java/de/bankenit/smartbank24kreditantragcityscoring/Smartbank24KreditantragCityScoringApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bankenit.smartbank24kreditantragcityscoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Smartbank24KreditantragCityScoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
